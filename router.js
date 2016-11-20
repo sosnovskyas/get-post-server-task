1ee97d3cd3a8493d3c2cc75779dc31c6b3d6a9c9
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-11-21 00:47:49 +0300

    создан модуль обработки запросов

---
+++ router.js
@@ -0,0 +1,82 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+const jade = require('jade');
+
+
+module.exports = (req, res) => {
+  const streamErrorHandler = error => {
+    let errRes = {};
+    switch (error.code) {
+      case 'ENOENT':
+        errRes.code = 404;
+        errRes.message = 'ERROR 404: File not found';
+        break;
+      default:
+        errRes.code = 500;
+        errRes.message = 'Server Error';
+        break;
+    }
+
+    res.statusCode = errRes.code;
+    res.end(errRes.message);
+  };
+
+  const downloadRouteHandler = (routePath) => {
+    const filename = routePath.replace(/\/files\//,'');
+    const file = fs.ReadStream(path.join(__dirname,'files', filename));
+
+    console.log('downloadRouteHandler', path.join(__dirname,'files', filename));
+    file.pipe(res);
+    file.on('error', streamErrorHandler);
+
+    res.on('close', () => {
+      file.destroy();
+    });
+  };
+
+  let pathname = decodeURI(url.parse(req.url).pathname);
+
+  switch (req.method) {
+    case 'GET':
+      switch (pathname) {
+        case '/':
+          // отдачу файлов следует переделать "правильно", через потоки, с нормальной обработкой ошибок
+          let template = new fs.ReadStream(path.join(__dirname, 'index.jade'));
+          template.on('readable', () => {
+            res.end(jade.render(template.read()));
+          });
+
+          template.on('error', streamErrorHandler);
+          break;
+
+        case String(pathname.match(/^\/assets\/.*/g)): {
+          const file = fs.ReadStream(path.join(__dirname, pathname));
+          file.pipe(res);
+          file.on('error', streamErrorHandler);
+          res.on('close', () => {
+            file.destroy();
+          });
+        }
+          break;
+
+        case String(pathname.match(/^\/files\/.*/g)):
+          downloadRouteHandler(pathname);
+          break;
+
+        default:
+          res.statusCode = 400;
+          res.end('плохой роут');
+      }
+      break;
+    case 'POST':
+      break;
+    case 'DELETE':
+      break;
+    default:
+      res.statusCode = 502;
+      res.end("Not implemented");
+      break;
+  }
+};
