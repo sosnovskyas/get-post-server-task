f603dfac5f1b2dc17c2a1b80bb27b9590c0ef37d
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-11-22 12:40:57 +0300

    обновлены скрипты фронтенда

--- assets/bundle.js
+++ assets/bundle.js
@@ -2,20 +2,27 @@
 const ulClickHandler = event => {
   const target = event.target;
 
+  fileResultElement.innerText = '...loading';
+
   if (target.value == 'открыть') {
     console.log(`открыть файл ${target.name}`);
     getFile(target.name)
       .then(result => {
-        console.log(result);
-        fileResultElement.innerText = result;
-      })
+          console.log(result);
+          fileResultElement.innerText = result;
+        },
+        err => fileResultElement.innerText = 'ERROR OPEN FILE: ' + err
+      )
   } else if (target.value == 'удалить') {
     console.log(`удалить файл ${target.name}`);
     if (confirm(`Удалить файл${target.name}?`)) {
       delFile(target.name)
         .then(result => {
-          console.log(result);
-        })
+            console.log(result);
+            fileResultElement.innerText = 'file delited ok';
+          },
+          err => fileResultElement.innerText = 'ERROR DELETE FILE: ' + err
+        )
     }
   }
 };
@@ -49,7 +56,7 @@ const postFile = file => {
   return new Promise(function (resolve, reject) {
 
     const xhr = new XMLHttpRequest();
-    xhr.open('POST', `http://localhost:8000/files`, true);
+    xhr.open('POST', `http://localhost:8000/files/${file.name}`, true);
 
     xhr.onload = function () {
       if (this.status == 200) {
@@ -120,56 +127,63 @@ const getAllFiles = () => {
   });
 };
 
+const refresh = () => {
+  fileResultElement.innerText = '...loading';
+  getAllFiles()
+    .then(files => {
+      const fileList = files.split(',');
+      let ul = document.createElement('ul');
+      ul.addEventListener('click', ulClickHandler);
+      for (let i = 0; i < fileList.length; i++) {
+        let li = document.createElement('li');
+        let span = document.createElement('span');
+
+        let buttonOpen = document.createElement('input');
+        buttonOpen.setAttribute('type', 'button');
+        buttonOpen.setAttribute('class', 'button-open');
+        buttonOpen.setAttribute('name', fileList[i]);
+        buttonOpen.setAttribute('value', 'открыть');
+
+        let buttonDel = document.createElement('input');
+        buttonDel.setAttribute('type', 'button');
+        buttonDel.setAttribute('class', 'button-del');
+        buttonDel.setAttribute('name', fileList[i]);
+        buttonDel.setAttribute('value', 'удалить');
+
+        span.innerText = fileList[i];
+
+
+        li.appendChild(span);
+        li.appendChild(buttonOpen);
+        li.appendChild(buttonDel);
+        ul.appendChild(li);
+      }
+
+      fileListElement.innerHTML = '';
+      fileListElement.appendChild(ul);
+      fileResultElement.innerText = 'ok';
+    });
+};
+
 let uploadData = document.querySelector('.upload-data');
 const fileListElement = document.querySelector('.file-list');
 const fileResultElement = document.querySelector('.file-result');
 
 function uploadHandler() {
-  console.log('uploadHandler', uploadData.value);
+  fileResultElement.innerText = '...loading';
   postFile(uploadData.files[0])
     .then(
       result => {
         console.log('uploadHandler: result', result);
+        fileResultElement.innerText = 'file uploaded successfully';
+        setTimeout(() => refresh(), 2000);
       },
-      console.error
-    )
+      error => {
+        fileResultElement.innerText = 'ERROR UPLOAD: ' + error;
+      }
+    );
 }
 
 document.querySelector('.upload').addEventListener('click', uploadHandler);
 
-
-getAllFiles()
-  .then(files => {
-    const fileList = files.split(',');
-    let ul = document.createElement('ul');
-    ul.addEventListener('click', ulClickHandler);
-    for (let i = 0; i < fileList.length; i++) {
-      let li = document.createElement('li');
-      let span = document.createElement('span');
-
-      let buttonOpen = document.createElement('input');
-      buttonOpen.setAttribute('type', 'button');
-      buttonOpen.setAttribute('class', 'button-open');
-      buttonOpen.setAttribute('name', fileList[i]);
-      buttonOpen.setAttribute('value', 'открыть');
-
-      let buttonDel = document.createElement('input');
-      buttonDel.setAttribute('type', 'button');
-      buttonDel.setAttribute('class', 'button-del');
-      buttonDel.setAttribute('name', fileList[i]);
-      buttonDel.setAttribute('value', 'удалить');
-
-      span.innerText = fileList[i];
-
-
-      li.appendChild(span);
-      li.appendChild(buttonOpen);
-      li.appendChild(buttonDel);
-      ul.appendChild(li);
-    }
-
-    fileListElement.innerHTML = '';
-    fileListElement.appendChild(ul);
-  });
-
-
+refresh();
